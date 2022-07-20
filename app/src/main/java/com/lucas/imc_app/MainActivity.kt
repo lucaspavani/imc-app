88ff8d2f46a534132350505ab87501a649c89048
lucaspavani <lucaspavanisantos@outlook.com>
2022-07-19 21:20:07 -0300

    Criação da o layout e função para o cálculo de IMC

--- app/src/main/java/com/lucas/imc_app/MainActivity.kt
+++ app/src/main/java/com/lucas/imc_app/MainActivity.kt
@@ -2,10 +2,35 @@ package com.lucas.imc_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListeners()
+    }
+
+    private fun setListeners(){
+        alturaEDT?.doAfterTextChanged { text ->
+        }
+        pesoEDT?.doOnTextChanged { text, _, _, _ ->
+        }
+        calcularBTN?.setOnClickListener{
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private fun calcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null){
+            val imc = peso / (altura * altura)
+            tituloTXT.text = "Seu IMC é:\n%.2f".format(imc)
+        }
     }
 }
